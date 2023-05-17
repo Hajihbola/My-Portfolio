581aff71f5e0a0a6f42e5b63fc6a685756420369
Blessing <ajibolablessing3@gmail.com>
2023-05-17 03:57:32 +0100

    My Portfolio

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 
-import profile from './profile.png';
-import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg-icons';
+import profile from './profile.jpg';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-	name: "Hashir",
+	name: "Blessing",
 	links: [
 		{
 			title: "About",
@@ -25,8 +25,8 @@ export const navigation = {
 	],
 }
 export const intro = {
-	title: "Hey, I'm Hashir",
-	description: "A Computer Engineer creating mobile apps and static websites.",
+	title: "Hey, I'm Odebunmi Blessing",
+	description: "A frontend software developer creating websites.",
 	image: profile.src,
 	buttons: [
 		{
@@ -36,7 +36,7 @@ export const intro = {
 		},
 		{
 			title: "Resume",
-			link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+			link: "https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:4eeb631c-32e7-45b3-bd23-5b03f81f09e8",
 			isPrimary: false,
 		},
 	],
@@ -45,9 +45,9 @@ export const intro = {
 export const about = {
 	title: "Who I am",
 	description: [
-		"I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-		"The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-		"When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
+		"I graduated from University of Ilorin in 2019 with a bachelor of science degree in Physiology. I kick started my professional career as a frontend software developer working onsite as an intern for a multinational agrocommodity company that has its branches in a couple of countries — AFEX.",
+		"The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping new developers, and supporting organizations to promote diversity in tech.",
+		"When I’m not programming, I focus on my hobbies which are: surfing the internet, creative skills such as wig making or revamping and event services amongst others, meeting people and growing my network.",
 	],
 }
 
@@ -55,13 +55,8 @@ export const work = {
 	title: "What I do",
 	cards: [
 		{
-			title: "Mobile App Development",
-			description: "I create pixel perfect iOS and Andriod apps using Flutter.",
-			icons: null,
-		},
-		{
 			title: "Web Development",
-			description: "I create responsive static websites using Reactjs.",
+			description: "I create responsive static websites using Reactjs, Typescript, JavaScript, HTML5, CSS3, TailwindCSS, Next.js mainly.",
 			icons: null,
 		}
 	],
@@ -71,53 +66,56 @@ export const projects = {
 	title: "Projects",
 	cards: [
 		{
-			title: "StarBook",
-			description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
+			title: "Workbench",
+			description: "An admin page view that shows, tracks information of agrocommodities' transactions and productivity throughout the month/year.",
 			icons: [
-				{
-					icon: faAppStore,
-					link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
-				},
 				{
 					icon: faGithub,
-					link: "https://github.com/hashirshoaeb/star_book",
+					link: "https://github.com/Hajihbola/Workbench.git",
 				},
 			]
 		},
 		{
-			title: "QuranTalk",
-			description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
+			title: "Car of the Year 2020",
+			description: "A static website that gives you information about the 2020 car of the year of an automobile company.",
 			icons: [
 				{
-					icon: faAppStore,
-					link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
-				},
-				{
-					icon: faGooglePlay,
-					link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
+					icon: faGithub,
+					link: "https://github.com/Hajihbola/Car-of-the-year.git",
 				},
 			]
 		},
 		{
-			title: "Portfolio",
+			title: "Josh Comeau",
+			description: "A static website that displays the Josh Comeau documentation.",
+			icons: [
+				{
+					icon: faGithub,
+					link: "https://github.com/Hajihbola/Josh-Comeau.git",
+				},
+			]
+		},
+	{	
+		title: "Portfolio",
 			description: "A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
 			icons: [
 				{
 					icon: faGithub,
-					link: "https://github.com/hashirshoaeb/portfolio",
+					link: "https://github.com/Hajihbola/My-Portfolio",
 				},
 			]
 		},
+
 	],
 }
 
 export const contact = {
 	title: "Get in touch",
-	description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
+	description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at ajibolablessing3@gmail.com.",
 	buttons: [
 		{
 			title: "Email Me",
-			link: "mailto:hashirshoaeb@gmail.com",
+			link: "ajibolablessing3@gmail.com",
 			isPrimary: true,
 		},
 		{
@@ -131,35 +129,27 @@ export const contact = {
 // SEARCH ENGINE 
 export const SEO = {
 	// 50 - 60 char  
-	title: "Hashir Shoaib | Computer Engineer | Flutter | Reactjs developer",
-	description: "I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
+	title: "Odebunmi Blessing | Frontend Software Developer| Reactjs developer",
+	description: "I create responsive websites for small, mediun, large scale businesses and individuals. I graduated from University of Ilorin in 2019 with a bachelor of science degree in Physiology.",
 	image: profile.src,
 }
 
 export const links = {
 	image: profile.src,
-	title: "@hashirshoaeb",
-	description: "Computer Engineer | Flutter | Reactjs Developer",
+	title: "@hajihbola",
+	description: "Frontend Software Developer | Reactjs Developer",
 	cards: [
 		{
 			title: "My website",
 			link: "https://hashirshoaeb.com/",
 		},
 		{
-			title: "QuranTalk App",
-			link: "https://www.qurantalk.app/",
-		},
-		{
-			title: "StarBook App",
-			link: "https://starbook.dev/",
-		},
-		{
 			title: "My GitHub",
-			link: "https://github.com/hashirshoaeb/",
+			link: "https://github.com/Hajihbola/",
 		},
 		{
 			title: "My LinkedIn",
-			link: "https://www.linkedin.com/in/hashirshoaeb/",
+			link: "https://www.linkedin.com/in/blessing-odebunmi-48aa001b3/",
 		},
 	]
 }
